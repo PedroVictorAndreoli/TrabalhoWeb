3a3eaa7108395c80195c077fa06df369aa8f8758
PedroVictorAndreoli <114033109+PedroVictorAndreoli@users.noreply.github.com>
2023-11-06 21:25:56 -0300

    Delete ExercicioWeb/src/main/java/org/example/service directory

--- ExercicioWeb/src/main/java/org/example/service/IContaService.java
+++
@@ -1,7 +0,0 @@
-package org.example.Service;
-
-import org.example.model.Conta;
-
-public interface IContaService extends ICrudService<Conta, Long> {
-
-}
--- ExercicioWeb/src/main/java/org/example/service/IMovimentacaoService.java
+++
@@ -1,7 +0,0 @@
-package org.example.Service;
-
-import org.example.model.Movimentacao;
-
-public interface IMovimentacaoService extends ICrudService<Movimentacao, Long> {
-
-}
