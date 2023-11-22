cff108f0a7a582969c42a14fc14846745d5ced6c
PedroVictorAndreoli <pvandreoli@gmail.com>
2023-11-21 21:43:49 -0300

    Agora o frontend ta no repositorio certo

---
+++ client/src/routes/AuthenticatedRoutes/index.tsx
@@ -0,0 +1,14 @@
+import AuthService from "@/services/AuthService";
+import { Navigate, Outlet } from "react-router-dom";
+
+export function AuthenticatedRoutes() {
+  const isAuthenticated = AuthService.isAuthenticated();
+
+  return isAuthenticated ? (
+    <>
+        <Outlet />
+    </>
+  ) : (
+    <Navigate to="/login" replace />
+  );
+}
