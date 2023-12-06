c03f0de6092c78ec16a067e1490a36039e0809be
cenci-ntido <105450250+cenci-ntido@users.noreply.github.com>
2023-12-06 20:18:13 -0300

    Geração do blowfish no backend

--- ExercicioWeb/src/main/java/org/example/Main.java
+++ ExercicioWeb/src/main/java/org/example/Main.java
@@ -1,12 +1,28 @@
 package org.example;
 
+import org.example.security.BlowFishKeyGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
         SpringApplication.run(Main.class, args);
+
+        BlowFishKeyGenerator b = new BlowFishKeyGenerator();
+
+        System.out.println(b.generateKey("chave"));
+
+
     }
 }
